2fa2cdf66e4993ac6c9dfc5df9ceba98b0deacff
sobia <sobiashahbaz66@yahoo.com>
2018-11-13 02:35:19 +0100

    Updates

--- precache-manifest.b077370987dec1eeaaa4aa49f04588e5.js
+++ precache-manifest.b077370987dec1eeaaa4aa49f04588e5.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/Shop-Fish/static/js/runtime~main.adce1b0d.js"
   },
   {
-    "revision": "a8ea829af00bec02c1ed",
-    "url": "/Shop-Fish/static/js/main.a8ea829a.chunk.js"
+    "revision": "7a75d4612ca9494e3d53",
+    "url": "/Shop-Fish/static/js/main.7a75d461.chunk.js"
   },
   {
     "revision": "60d673d13546726df1f4",
     "url": "/Shop-Fish/static/js/1.60d673d1.chunk.js"
   },
   {
-    "revision": "a8ea829af00bec02c1ed",
+    "revision": "7a75d4612ca9494e3d53",
     "url": "/Shop-Fish/static/css/main.b38dce53.chunk.css"
   },
   {
-    "revision": "824669063d365cb142d6b3f460c72d79",
+    "revision": "205a5da87a9a57ab30bf5fa5268b1e5d",
     "url": "/Shop-Fish/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Shop-Fish/precache-manifest.90e4b7e1213f93ca79a79ce9c722de8f.js"
+  "/Shop-Fish/precache-manifest.b077370987dec1eeaaa4aa49f04588e5.js"
 );
 
 workbox.clientsClaim();
