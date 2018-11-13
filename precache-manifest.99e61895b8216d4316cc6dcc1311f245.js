8a0b574246c9c2425bc3904093880d30b859ae3c
sobia <sobiashahbaz66@yahoo.com>
2018-11-13 02:15:21 +0100

    Updates

---
+++ precache-manifest.99e61895b8216d4316cc6dcc1311f245.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4653ce4c002f20afca2",
+    "url": "/fish-shop/static/js/runtime~main.c4653ce4.js"
+  },
+  {
+    "revision": "dde344553e11fb7bf20e",
+    "url": "/fish-shop/static/js/main.dde34455.chunk.js"
+  },
+  {
+    "revision": "fb1ed9e818774c64e1df",
+    "url": "/fish-shop/static/js/1.fb1ed9e8.chunk.js"
+  },
+  {
+    "revision": "dde344553e11fb7bf20e",
+    "url": "/fish-shop/static/css/main.b38dce53.chunk.css"
+  },
+  {
+    "revision": "f2de247e71969a5dd727cc460bc0499d",
+    "url": "/fish-shop/index.html"
+  }
+];
