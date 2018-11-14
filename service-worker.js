b062b066dc36cda85a3bb43f683028f931c34aaa
sobia <sobiashahbaz66@yahoo.com>
2018-11-14 21:57:14 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Shop-Fish/precache-manifest.d18432da4abce69785e1abda756ced74.js"
+  "/Shop-Fish/precache-manifest.7ef5228381afa84ffde4e1e421a03654.js"
 );
 
 workbox.clientsClaim();
