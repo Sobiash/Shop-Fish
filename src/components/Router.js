9a6df1ca4832ed415af26597e16de3273db455b1
sobia <sobiashahbaz66@yahoo.com>
2018-11-17 17:26:33 +0100

    Not Found Page added

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Store from "./Store";
 import App from "./App";
+import NotFound from "./NotFound";
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Store} />
       <Route path="/store/:storeId" component={App} />
+      <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
 );
